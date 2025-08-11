2b5c8187240ac85dd070c805aac7fcd1df2da3df
Dmytro <dimkanividimka4@gmail.com>
2025-08-11 19:18:02 +0300

    Solution

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM python:3.12.11-alpine
+FROM python:3.12.11-slim
 LABEL maintainer="dimkanividimka@gmail.com"
 ENV PYTHONUNBUFFERED 1
-WORKDIR = app/
+WORKDIR app/
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 COPY . .
--- app/main.py
+++ app/main.py
@@ -4,20 +4,27 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+URL = "http://api.weatherapi.com/v1/current.json"
+
+CITY = "Paris"
+
 
 def get_weather() -> None:
     api_key = os.environ.get("API_KEY")
-    payload = {"key": api_key,
-               "q": "Paris"}
-    res = requests.get(
-        "http://api.weatherapi.com/v1/current.json",
-        params=payload
-    )
-    json_data = res.json()
-    print(f"Зараз в парижі: {json_data["current"]["condition"]["text"]}")
-    print(f"температура повітря {json_data["current"]["temp_c"]}")
-    print(f"швидкість вітру {json_data["current"]["wind_kph"]}")
-    print(f"напрямок вітру {json_data["current"]["wind_dir"]} ")
+    if api_key:
+        payload = {"key": api_key,
+                   "q": CITY}
+        res = requests.get(
+            URL,
+            params=payload
+        )
+        json_data = res.json()
+        print(f"Зараз в парижі: {json_data["current"]["condition"]["text"]}")
+        print(f"температура повітря {json_data["current"]["temp_c"]}")
+        print(f"швидкість вітру {json_data["current"]["wind_kph"]}")
+        print(f"напрямок вітру {json_data["current"]["wind_dir"]} ")
+    else:
+        print("API KEY NOT FOUND")
 
 
 if __name__ == "__main__":
