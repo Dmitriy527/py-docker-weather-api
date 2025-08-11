71db2294f20b1e70dbf71e00883d53d9923d87ef
Dmytro <dimkanividimka4@gmail.com>
2025-08-11 19:36:47 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -19,10 +19,10 @@ def get_weather() -> None:
             params=payload
         )
         json_data = res.json()
-        print(f"Зараз в парижі: {json_data["current"]["condition"]["text"]}")
-        print(f"температура повітря {json_data["current"]["temp_c"]}")
-        print(f"швидкість вітру {json_data["current"]["wind_kph"]}")
-        print(f"напрямок вітру {json_data["current"]["wind_dir"]} ")
+        print(f"Зараз в парижі: {json_data['current']['condition']['text']}")
+        print(f"температура повітря {json_data['current']['temp_c']}")
+        print(f"швидкість вітру {json_data['current']['wind_kph']}")
+        print(f"напрямок вітру {json_data['current']['wind_dir']} ")
     else:
         print("Error: API key is missing.")
         exit(1)
